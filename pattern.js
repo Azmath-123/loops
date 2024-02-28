2cf53265b7fa2b4754559f8d2627ba02b75ca5e1
Azmath-123 <azmathsoheb@gmail.com>
2024-02-28 22:53:08 +0530

    first commit

--- pattern.js
+++ pattern.js
@@ -1,14 +1,26 @@
-var num ="";
-for(i=0; i<10; i++){
-    num +=i;
-    console.log(num);
+ var num ="";
+ for(i=1; i<10; i++){
+     num +=i;
+     console.log(num);
 }
 
+ var string="";
+ for(i=0; i<=4; i++) {  
+ 	string+= "* ";  
+   console.log(string+"\n");  
 
 
-var str = 'A ';
-for(i=0; i<=5; i++) {  
-    str+= i;
-    console.log(str);
+
+   
+var num="";
+
+for(i=65; i<=67; i++) {
+    for(j=65; j<=i; j++) {
+        num+=String.fromCharCode(j);
 }
+num += '/n';
+    
+}
+console.log(num);
+
 
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+var string="";
+for(i=4 ; i<=1 ; i--) {  
+	string+= "* ";  
+    console.log(string+"\n");  
+
+}
